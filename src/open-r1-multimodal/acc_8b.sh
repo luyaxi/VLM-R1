b69ce4cb48fc3daa6976ff3d747dc54214a4cd33
luyaxi <luyaxi@live.com>
2025-03-31 09:44:27 +0800

    update script

--- src/open-r1-multimodal/acc_8b.sh
+++ src/open-r1-multimodal/acc_8b.sh
@@ -7,7 +7,7 @@ cd `dirname $0`
 
 # RUN_NAME="MiniCPMV-HW-THOUGHT-7B-GRPO-1120px-8s-lr"
 # RUN_NAME="MiniCPMV-HW-E-THOUGHT-7B-GRPO-1120px-8s-lr"
-RUN_NAME="MiniCPMV-HW-7B-GRPO-1120px-8s"
+RUN_NAME="MiniCPMV-HW-7B-GRPO-1120px-4s"
 
 set -ex
 TOKENIZERS_PARALLELISM=false CUDA_DEVICE_MAX_CONNECTIONS=1 UCX_NET_DEVICES=bond0 GLOO_SOCKET_IFNAME=bond0 NCCL_SOCKET_IFNAME=bond0 NCCL_IB_HCA="mlx5_2,mlx5_3,mlx5_5,mlx5_6" WANDB_PROJECT=CPM-RFT accelerate launch \
@@ -19,7 +19,7 @@ TOKENIZERS_PARALLELISM=false CUDA_DEVICE_MAX_CONNECTIONS=1 UCX_NET_DEVICES=bond0
     --max_prompt_length 2048 \
     --max_completion_length 96 \
     --max_line_res 1120 \
-    --num_generations 8 \
+    --num_generations 4 \
     --num_iterations 1 \
     --per_device_train_batch_size 4 \
     --gradient_accumulation_steps 8 \
@@ -35,7 +35,7 @@ TOKENIZERS_PARALLELISM=false CUDA_DEVICE_MAX_CONNECTIONS=1 UCX_NET_DEVICES=bond0
     --tune_vision true \
     --gather_deepspeed3_params true \
     --bf16 \
-    --beta 0.01 \
+    --beta 0.04 \
     --data_seed 42 \
     --report_to wandb \
     --gradient_checkpointing true \
